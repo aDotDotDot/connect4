3c7bb52f530135decfd25ad5c14c950c9bd77327
DotDotDot <nonozebest@gmail.com>
2018-08-31 21:18:17 +0200

    comments and file definition

--- pi/pi.js
+++ pi/pi.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
-//const backwardsStream = require('fs-backwards-stream')
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
 const moment = require('moment');
 const tStart = moment().valueOf();;
 console.log(`Started at ${moment(tStart).format()}`);
-//const buf1 = Buffer.alloc(65536, 0, 'hex');
-//const txtSrch = "coucou"
+const filePath = __dirname + '/pi-billion.txt';
 let piFile;
-let stats = fs.statSync(__dirname + '/pi-billion.txt');
+let stats = fs.statSync(filePath);
 const fileSizeInBytes = {'pi':stats.size};
 
 const ascii_to_hexa = (str) => {
@@ -24,21 +22,18 @@ const hexa_to_dec = (hexStr) => {
     return parseInt(hexStr, 16);
 }
 const readBytes = (position, size) => {
-    //console.log(position,size);
     let bufferTmp = Buffer.alloc(size, '0', 'ascii');
-    //console.log(bufferTmp);
     return new Promise((resolve, reject) => {
         if(position>fileSizeInBytes.pi)
             reject('End of file');
         fs.read(piFile, bufferTmp, 0, size, position, (err, bytesRead, bufferTmp) => {
-            //console.log(err, bytesRead, bufferTmp);
             if(err)
                 reject(err);
             resolve(bufferTmp);
         });
     });
 };
-fs.open(__dirname + '/pi-billion.txt', 'r', (err, fd) => {
+fs.open(filePath, 'r', (err, fd) => {
     if(err)
         console.warn(err);
     piFile = fd;
@@ -57,7 +52,7 @@ const findDecimalInPi = (decNb, pStart, pEnd) => {
 
 const findBufferInPi = (bufTxt, pStart, pEnd) => {
     let bufLength = bufTxt.length;
-    let position = 2;//on évite le 3.
+    let position = 2;//it begins with 3.
     if(pStart && pStart > 1)
         position = pStart;
     if(position%100000 == 0)
@@ -67,7 +62,6 @@ const findBufferInPi = (bufTxt, pStart, pEnd) => {
             if(bufFound.equals(bufTxt)){
                 console.log(`Found at pos : ${position}`);
                 return position;
-                //break;
             }
             else{
                 position++;
@@ -85,12 +79,11 @@ const findBufferInPi = (bufTxt, pStart, pEnd) => {
 
 const findBufferInPiReverse = (bufTxt, pStart, pEnd) => {
     let bufLength = bufTxt.length;
-    let position = (fileSizeInBytes.pi);//part de la fin
+    let position = (fileSizeInBytes.pi);//from the end
     if(pStart && pStart <= fileSizeInBytes.pi)
         position = pStart;
     if(!pEnd)
         pEnd = (fileSizeInBytes.pi/2);
-    //console.log(position);
 
     if(position%100000 == 0)
         console.log(`Position ${position}`);
@@ -99,7 +92,6 @@ const findBufferInPiReverse = (bufTxt, pStart, pEnd) => {
             if(bufFound.equals(bufTxt)){
                 console.log(`Found at pos : ${position}`);
                 return position;
-                //break;
             }
             else{
                 position--;
@@ -114,47 +106,29 @@ const findBufferInPiReverse = (bufTxt, pStart, pEnd) => {
     return -1;
 };
 
-//console.log(ascii_to_hexa(txtSrch));
-//console.log(hexa_to_dec(ascii_to_hexa(txtSrch)));
-
 console.log(`File size : ${fileSizeInBytes.pi}`);
 
 eventEmitter.on('ready', (evt) => {
-    /*readBytes(65534, 10).then((dd)=>{
 
-    })*/
-    //findTextInPi('>6');
-    //findTextInPi('#V');
-    //findDecimalInPi(21101);
-    //findDecimalInPi(77274, 999970000);
-    //findBufferInPiReverse(Buffer.from('77274'));
-    /*fs.readFile(__dirname + '/cat.jpg', (err, buffer) => {
-        console.log(buffer.length);
-        findBufferInPi(buffer);
-    });*/
 });
 
 const findTextInPiChunk = (buf) => {
     return new Promise( (resolve, reject) => {
-        let readStream = fs.createReadStream(__dirname + '/pi-billion.txt', 'utf8');
-        let readStreamShifted = fs.createReadStream(__dirname + '/pi-billion.txt', {encoding:'utf8', start:(65536-buf.length)});
+        let readStream = fs.createReadStream(filePath, 'utf8');
+        let readStreamShifted = fs.createReadStream(filePath, {encoding:'utf8', start:(65536-buf.length)});
         let totalSize = 0;
         let totalSizeShifted = (65536-buf.length);
         let bufSearch = Buffer.from(buf);
         let result = {search: buf, positions:new Set(), time: null};
         readStream.on('data', (chunk) => {
-            //console.log(chunk.toString('hex'));  
             if(totalSize < (buf.length + (fileSizeInBytes.pi / 2))){
                 readBytes(fileSizeInBytes.pi - totalSize - buf.length, 65536).then((reverseBuf) => {
-                    //console.log(reverseBuf.toString('ascii'));
                     if(reverseBuf.includes(bufSearch)){
                         let evt = {chunk:reverseBuf.toString('ascii'), search:buf, size:(fileSizeInBytes.pi - totalSize + 65536 - buf.length)};
                         result.positions.add(evt.chunk.indexOf(evt.search)+evt.size);
                         if(!result.time)
                             result.time = moment().valueOf() - tStart;
                         eventEmitter.emit('foundEnd', evt);
-                        /*readStream.close();
-                        readStreamShifted.close();*/
                     }
                 }).catch(() => {});
             }
@@ -168,16 +142,8 @@ const findTextInPiChunk = (buf) => {
                 else
                     resolve(result);
                 readStream.close();
-                /*readStream.close();
-                readStreamShifted.close();*/
             }else{
                 totalSize+=chunk.length;
-                /*if(totalSize > ((fileSizeInBytes.pi/2) + 2*65536 + buf.length)){
-                    if(result.positions.size == 0)
-                        reject(result);
-                    else
-                        resolve(result);
-                }*/
             }
         }).on('end', () => {
             if(result.positions.size == 0)
@@ -186,7 +152,6 @@ const findTextInPiChunk = (buf) => {
                 resolve(result);
         });
         readStreamShifted.on('data', (chunk) => {
-            //console.log(chunk.toString('hex'));  
             if(chunk.includes(buf)){
                 let evt = {chunk:chunk, search:buf, size:totalSizeShifted};
                 result.positions.add(evt.chunk.indexOf(evt.search)+evt.size);
@@ -197,19 +162,9 @@ const findTextInPiChunk = (buf) => {
                 else
                     resolve(result);
                 readStreamShifted.close();
-                /*readStream.close();
-                readStreamShifted.close();*/
             }else{
                 totalSizeShifted+=chunk.length;
-                //console.log("Not in this chunk");
-                /*if(totalSizeShifted > ((fileSizeInBytes.pi/2) + 65536 + buf.length)){
-                    if(result.positions.size == 0)
-                        reject(result);
-                    else
-                        resolve(result);
-                    readStreamShifted.close();*/
-                }
-            //}
+            }
         }).on('end', () => {
             if(result.positions.size == 0)
                 reject(result);
@@ -231,21 +186,3 @@ bb='55715171395115275045519';
 findTextInPiChunk(bb).then(e=>{console.log(e);}).catch(e=>{console.log(nope);});
 bb='03041988';
 findTextInPiChunk(bb).then(e=>{console.log(e);}).catch(e=>{console.log(nope);});
-
-/*eventEmitter.on('found', (evt) => {
-    //console.log('FOUND');
-    let tEnd = moment().valueOf();
-    let pos = evt.chunk.indexOf(evt.search);
-    console.log(`Found ${evt.search} at position ${(pos+evt.size)} in ${tEnd - tStart}ms`);
-    let subs = evt.chunk.substring(Math.max(0, pos-10), pos+10+evt.search.length+10);
-    console.log(`In this substring : ${subs.replace(evt.search,'*'+evt.search+'*')}`);
-});*/
-
-/*eventEmitter.on('foundEnd', (evt) => {
-    //console.log('FOUND');
-    let tEnd = moment().valueOf();
-    let pos = evt.chunk.indexOf(evt.search);
-    console.log(`Found (may not be the first) ${evt.search} at position ${(pos+evt.size)} in ${tEnd - tStart}ms`);
-    let subs = evt.chunk.substring(Math.max(0, pos-10), pos+10+evt.search.length+10);
-    console.log(`In this substring : ${subs.replace(evt.search,'*'+evt.search+'*')}`);
-});*/
